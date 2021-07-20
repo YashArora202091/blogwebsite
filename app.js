9c6ebb4f89b9a6184fc475c5cb98688dcbb5d48a
YashArora202091 <63581376+YashArora202091@users.noreply.github.com>
2021-07-20 17:01:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "This is a blog website created by Yash Arora, an aspiring full stack web developer using modern technologies such as HTML,CSS and Javascript for front end incorporating their libraries like Bootstrap and Jquery , the backend part is done using Node.js with the help of Express.js and the database is created using mongodb and mongoose. I hope you like the website , have fun making your own blogs and do contact me for any suggestions or comments , my contact information  is available on the navbar contact us .To compose a new blog post just add /compose on the url. ";
+const aboutContent = "The website is created by Yash Arora , a pre-final year student at Indian Institute of Technology Guwahati, I am an aspiring full stack web developer , for more information about me , you can checkout this website .";
+const contactContent = "Here are my contact details:";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-yash:Test123@cluster0.tn7xz.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
+});
